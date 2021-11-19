25682697d3d17bf565f65ee676311250647ccdcd
vishal-c-addweb <vishal.c@addwebsolution.in>
2021-11-19 16:22:48 +0530

    demo

--- src/types/Payload.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Payload Object to be signed and verified by JWT. Used by the auth middleware to pass data to the request by token signing (jwt.sign) and token verification (jwt.verify).
- * @param userId:string
- * @param isAdmin:boolean
- */
-type payload = { userId: string,isAdmin:boolean };
-
-export default payload;
